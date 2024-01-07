c1822300eeb5fb561ca4c60169d664106ec7ba05
= <kirklimushin@gmail.com>
2024-01-07 21:16:32 +0300

    Initialization commit

---
+++ grad_cam.py
@@ -0,0 +1,148 @@
+import numpy 
+import cv2 
+from torch import nn 
+import torch 
+import matplotlib.pyplot as plt
+from torchvision import models
+import typing
+
+class GradCAM(object):
+    
+    """
+    Gradient-based Class Activation Mapping 
+    (method for visual interpretation of the Convolutional Neural Networks)
+    
+    Parameters:
+    -----------
+        - activations (typing.List) - list for storing network layer activations
+        - grad (typing.List) - list for storing output gradients of the last convolutional layer
+    """
+
+    def __init__(self):
+        self.activations = []
+        self.grads = []
+
+    @staticmethod
+    def visualize_grad_cam_map(actual_img: numpy.ndarray, cam_map: numpy.ndarray):
+        """
+        Function visualizes map,
+        generated from grad-cam.
+        """
+        heatmap = cv2.applyColorMap(
+            cam_map.astype(numpy.uint8), 
+            colormap=cv2.COLORMAP_JET
+        )
+        visual_map = (actual_img * 0.7 + heatmap * 0.3).astype(numpy.uint8)
+        plt.imshow(visual_map)
+
+    def _track_forward_hook(self, module: nn.Module, input: torch.Tensor, output: torch.Tensor):
+        self.activations.append(output)
+
+    def _track_backward_hook(self, module: nn.Module, input_grad: torch.Tensor, output_grad: torch.Tensor):
+        self.grads.append(output_grad[0])
+
+    def _register_tracking_hooks(self, last_layer: nn.Module):
+
+        self.forward_hook = last_layer.register_forward_hook(hook=self._track_forward_hook)
+        self.backward_hook = last_layer.register_backward_hook(hook=self._track_backward_hook)
+
+    def _remove_hooks(self):
+        """
+        Removes registered 
+        forward and backward hooks
+        """
+        self.forward_hook.remove()
+        self.backward_hook.remove()
+        
+    def compute_grad_cam(
+        self, 
+        network: nn.Module, 
+        input_img: numpy.ndarray,
+        target_label: int,
+    ):
+        """
+        Function computes gradient-based
+        Class Activation Mapping 
+        
+        Parameters:
+        -----------
+        
+        network - convolutional-based neural network for making predictions
+        input_img - (numpy.ndarray) RGB image to be interpreted
+        target_label - class of the object you want to observe on the image
+        """
+
+        # converting image to C-like ordered array 
+        # (NOTE: gives error, if you remove this line) 
+
+        img = input_img.copy()
+        img = cv2.cvtColor(input_img, cv2.COLOR_BGR2RGB)
+        img = numpy.ascontiguousarray(img)
+
+        # converting to torch representation and expand dimension for the batch
+        torch_img = torch.from_numpy(   
+            img.astype(numpy.uint8)
+        ).permute(2, 0, 1).unsqueeze(0)
+        
+        # registering hooks for capturing activations and gradients
+        last_layer = network.layer4[-1]
+        self._register_tracking_hooks(last_layer=last_layer)
+        
+        # running image through the network (training mode)
+        predictions = network(torch_img.float())
+
+        network.zero_grad()
+        predictions[0, target_label].backward()
+
+        # obtaining gradients and feature maps
+        img_grads = self.grads[0].cpu().data.numpy().squeeze()
+        feature_maps = self.activations[0].cpu().data.numpy().squeeze()
+
+        # calculating weights for each feature map
+        res_grad = img_grads.reshape([img_grads.shape[0], -1])
+        weights = numpy.mean(res_grad, axis=1)
+
+            # calculating class activation map (CAM)
+        cam = numpy.zeros(shape=img_grads.shape[1:])
+        
+        for idx, weight in enumerate(weights):
+            cam += weight * feature_maps[idx, :]
+            
+        # normalizing cam to be in range [0, 255]
+        
+        cam = (cam > 0) * cam
+        cam = cam/cam.max()*255
+        
+        # resizing image to the original shape of the image
+        
+        resized_cam = cv2.resize(
+            src=cam, 
+            dsize=(input_img.shape[1], input_img.shape[0]), 
+            interpolation=cv2.INTER_CUBIC
+        )
+        
+        self._remove_hooks()
+        return resized_cam
+
+def test_cam():
+
+    input_img = cv2.imread("./test_cat_dog_img.jpeg", 1)
+    input_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2RGB)
+    input_img = cv2.resize(input_img, dsize=(512, 512), interpolation=cv2.INTER_CUBIC)
+    
+    network = models.resnet50(pretrained=True)
+
+    cam = GradCAM()
+
+    cam_mask = cam.compute_grad_cam(
+        network=network,
+        input_img=input_img,
+        target_label=281,
+    )
+    
+    print(cam_mask)
+
+    # visualizing computed grad-based CAM mask
+    cam.visualize_grad_cam_map(actual_img=input_img, cam_map=cam_mask)
+
+test_cam()
---
+++ integrated_gradients.py
@@ -0,0 +1,69 @@
+import numpy 
+import torch
+from torch import nn
+import typing
+import cv2
+
+def compute_gradient(model: nn.Module, img: torch.Tensor, loss: typing.Callable, target: int):
+    """
+    Function returns
+    """
+    img.requires_grad = True
+    logit = model(img)[0, target]
+    logit.backward()
+    img.requires_grad = False
+    return img.grad.data.squeeze(0)
+    
+def normalize_gradient(img: torch.Tensor, percentile: float):
+    if percentile > 1.0 or (percentile < 0):
+        raise ValueError(msg='invalid percentile')
+    return img / numpy.percentile(a=img.numpy(), q=percentile)
+
+def get_baseline(input_img: numpy.ndarray, baseline_type, **kwargs):
+
+    if baseline_type == 'black':
+        return numpy.zeros_like(input_img).astype(numpy.uint8)
+
+    if baseline_type == 'gaussian':
+        return numpy.asarray(cv2.GaussianBlur(
+            input_img, 
+            sigmaX=kwargs.get("sigmaX", 2),
+            sigmaY=kwargs.get("sigmaY", 2)
+        ))
+    raise ValueError('invalid baseline type.')
+
+def integrated_gradients(
+    network: nn.Module,
+    input_img: numpy.ndarray, 
+    actual_label: int,
+    loss_function: nn.Module,
+    n_steps: int, 
+    baseline_type: typing.Literal['gaussian', 'black']
+):
+    baseline = torch.from_numpy(get_baseline(input_img, baseline_type)).permute(2, 0, 1).unsqueeze(0)
+    input_img = torch.from_numpy(input_img.astype(numpy.uint8)).permute(2, 0, 1).unsqueeze(0)
+
+    path = [baseline + alpha * (input_img - baseline) for alpha in numpy.linspace(0, 1, num=n_steps)]
+    grads = [compute_gradient(model=network, img=img, loss=loss_function, target=actual_label) for img in path]
+    norm_grads = [normalize_gradient(grad, percentile=0.98) for grad in grads[:-1]]
+    int_grads = (input_img - baseline) * torch.cat(norm_grads[:-1]).mean(axis=0, keepdim=True)
+    return int_grads.squeeze(0).permute(1, 2, 0)
+
+import matplotlib.pyplot as plt
+import torchvision.models 
+
+img = cv2.imread("../test_input_2.jpeg", cv2.IMREAD_UNCHANGED).astype(numpy.uint8)
+model = torchvision.models.resnet18(pretrained=True)
+model.eval()
+
+grads = integrated_gradients(
+    network=model,
+    input_img=img, 
+    actual_label=1, 
+    loss_function=nn.CrossEntropyLoss(), 
+    n_steps=100,
+    baseline_type='black',
+)
+
+def application_string(self, remote_container: str, options: typing.Dict[str, int]):
+    pass
